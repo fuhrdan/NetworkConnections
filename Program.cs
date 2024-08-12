1246f589c4a4457bf8abece4c9e54b81e18bb77d
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-08-12 10:29:48 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿//*****************************************************************************
+//** Network Connections                                                     **
+//** A simple project to show available network connections.           -Dan  **
+//*****************************************************************************
+
+using System;
+using System.Net.NetworkInformation;
+
+namespace NetworkConnections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Available Network Connections:");
+            Console.WriteLine("-------------------------------");
+
+            // Get all network interfaces (network adapters) on the machine
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface network in networkInterfaces)
+            {
+                Console.WriteLine("Name: " + network.Name);
+                Console.WriteLine("Description: " + network.Description);
+                Console.WriteLine("Status: " + network.OperationalStatus);
+
+                // Get the IP properties of the network interface
+                IPInterfaceProperties ipProperties = network.GetIPProperties();
+
+                // Display unicast addresses (IP addresses)
+                foreach (UnicastIPAddressInformation ip in ipProperties.UnicastAddresses)
+                {
+                    Console.WriteLine("IP Address: " + ip.Address);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
